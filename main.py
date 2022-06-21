a83aa30f415414c8e435ea4f146e875f3feccc4a
Michael Schori <michael.schori@gmx.ch>
2022-06-21 10:39:42 +0200

    Created new api.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+
+
+class Time(BaseModel):
+    scanner_code: int
+    scanner_password: str
+    tag_id: int
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/time/", status_code=status.HTTP_201_CREATED)
+async def create_item(item: Time):
+    if item.scanner_code is not '789456123':
+        raise HTTPException(status_code=401, detail='Scanner-Code not found.')
+    if item.scanner_password is not 'Sml12345':
+        raise HTTPException(status_code=403, detail="Invalid Scanner-Password.")
+    if item.tag_id is not '42':
+        raise HTTPException(status_code=404, detail='No Tag with this ID found.')
+    return item
